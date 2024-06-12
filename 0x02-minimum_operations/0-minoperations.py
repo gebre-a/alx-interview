4b22166905dc530e317ed0f3d35288aa3a79edf3
gebre-a <kidanalemayehu12@gmail.com>
2024-06-12 04:58:26 -0700

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-
 """
-    A method that determines the number of minmum operations given n characters
-
+Main file for testing
 """
 
-def minOperations(n):
-    """
-        A function that calculates the fewest number of operations needed to giv        e a result of exactly n H characters in a file
-        args: n: Number of characters to be displayed
+minOperations = __import__('0-minoperations').minOperations
 
-        return:
-                number of min operations
-    """
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-    now = 1
-    start = 0
-    count = 0
-    while now < n:
-        rem = n - now
-        if ( rem % now == 0):
-            start = now
-            now += start
-            count += 2
-        else:
-            now += start
-            count += 1
-    return count
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
