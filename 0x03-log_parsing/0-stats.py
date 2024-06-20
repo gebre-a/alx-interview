f94baaceec3e774e12961dc7a4bfcee0143dc2e8
gebre-a <kidanalemayehu12@gmail.com>
2024-06-20 01:10:12 -0700

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python3
-"""Log Parser"""
+"""
+log parsing
+"""
 
-from sys import stdin
-
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
-tot_size = 0
-count = 0
+import sys
+import re
 
 
-def printStatistics():
-    print('File size: {}'.format(tot_size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-try:
-    for line in stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in status.keys():
-                status[code] += 1
-            tot_size += size
-            count += 1
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-        if count == 10:
-            count = 0
-            printStatistics()
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-except Exception as err:
-    pass
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-finally:
-    printStatistics()
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
